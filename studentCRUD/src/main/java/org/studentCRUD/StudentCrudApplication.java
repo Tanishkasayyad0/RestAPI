35b1b87b05ec1ef64c1637176f54a65815f1f335
Tanishkasayyad0 <tanishkasayyad852@gmail.com>
2026-09-05 19:49:54 +0530

    this is my first commit on intellij

---
+++ studentCRUD/src/main/java/org/studentCRUD/StudentCrudApplication.java
@@ -0,0 +1,17 @@
+package org.studentCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(StudentCrudApplication.class, args);
+
+        System.out.println("hello");
+    }
+
+}
